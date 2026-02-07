f7160bd2e369668f077a1d2d705aa4b36d2b8dcb
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-07 17:57:38 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/nazifanishat/sg_grocery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nazifanishat.sg_grocery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sg_grocery/view/splash/splash_page.dart';
+
+void main (){
+runApp(MyApp());
+
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: true,
+      home: SplashPage(),
+
+    );
+  }
+}
