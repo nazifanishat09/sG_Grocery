77a9dbee86af74842df1768dfba33330788bee02
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-08 20:41:18 +0600

    slider

--- lib/view/HomeUi/homePage.dart
+++ lib/view/HomeUi/homePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sg_grocery/view/HomeUi/slider_ui.dart';
 
 import '../widget/searchBer.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,6 +12,12 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List<String> sl = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHq8LzgbCl1y0TKSprZhiI94EfrQc_yzJxQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9oGtW-OcPE9oEKhTlgKad_MGo_nwD3780uA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9uk-ZY8Omaa7owVLym-umqKZ1IF776tLmQ&s",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +25,13 @@ class _HomepageState extends State<Homepage> {
         padding: const EdgeInsets.all(10),
         child: Column(
           children: [
-            SizedBox(height: 30,),
+            SizedBox(height: 30),
             SearchBerWidget(),
+            SizedBox(height: 20),
+            SliderWidget(sl: sl),
           ],
         ),
-      )
+      ),
     );
   }
 }
-
-
---
+++ lib/view/HomeUi/slider_ui.dart
@@ -0,0 +1,34 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class SliderWidget extends StatelessWidget {
+  const SliderWidget({
+    super.key,
+    required this.sl,
+  });
+
+  final List<String> sl;
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(height:154,viewportFraction: .9,autoPlay: true),
+      items: sl.map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image:DecorationImage(image: NetworkImage(i),
+                    fit: BoxFit.fill),
+              ),
+
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
