c470ac11233bae85f2cac3846c1bc75ce7c6acd1
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-09 11:49:59 +0600

    Card widget

---
+++ lib/view/HomeUi/card1.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../widget/custom_text.dart';
+
+
+
+class CardWidget1 extends StatelessWidget {
+  const CardWidget1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: ListView.builder(      clipBehavior: Clip.none,
+
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) => Stack(
+            alignment: Alignment.topCenter,
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                margin: EdgeInsets.all(5),
+                // height: 88,
+                width: 87,
+                decoration: BoxDecoration(
+                  color: Color(0xffF2FCF4),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: 38,
+                      width: 87,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+
+                        color: Color(0xff55AB60),
+                      ),
+                      child: Center(
+                        child: CustomTextWidget(
+                          title: "Groecries",
+                          fs: 12,
+                          fw: FontWeight.w600,
+                          fc: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 60,
+                left: 10,
+                child: Image.asset("assets/apple.png"),
+
+                // child: Image.network(
+                //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjNSiND1mtJ4c1x5AHwWfh1JhJVDR2042FhA&s",
+                //   width: 75,
+                //   height: 65,
+                // ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/HomeUi/homePage.dart
+++ lib/view/HomeUi/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sg_grocery/view/HomeUi/card1.dart';
 import 'package:sg_grocery/view/HomeUi/slider_ui.dart';
 import 'package:sg_grocery/view/widget/custom_text.dart';
 
@@ -52,68 +53,7 @@ class _HomepageState extends State<Homepage> {
               ),
               SizedBox(height: 40),
 
-              SizedBox(
-                height: 120,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: ListView.builder(      clipBehavior: Clip.none,
-
-                    itemCount: 10,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (BuildContext context, int index) => Stack(
-                      alignment: Alignment.topCenter,
-                      clipBehavior: Clip.none,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.all(5),
-                          // height: 88,
-                          width: 87,
-                          decoration: BoxDecoration(
-                            color: Color(0xffF2FCF4),
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Container(
-                                height: 38,
-                                width: 87,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                    bottomRight: Radius.circular(10),
-                                    bottomLeft: Radius.circular(10),
-                                  ),
-
-                                  color: Color(0xff55AB60),
-                                ),
-                                child: Center(
-                                  child: CustomTextWidget(
-                                    title: "Groecries",
-                                    fs: 12,
-                                    fw: FontWeight.w600,
-                                    fc: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 60,
-                          left: 10,
-                          child: Image.asset("assets/apple.png"),
-
-                          // child: Image.network(
-                          //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjNSiND1mtJ4c1x5AHwWfh1JhJVDR2042FhA&s",
-                          //   width: 75,
-                          //   height: 65,
-                          // ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              CardWidget1(),
               SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
