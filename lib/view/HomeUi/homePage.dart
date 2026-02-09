edeb8dcad7c0687405d2ebed690a968b5fbcf1c0
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-09 11:40:21 +0600

    top categories

--- lib/view/HomeUi/homePage.dart
+++ lib/view/HomeUi/homePage.dart
@@ -53,58 +53,64 @@ class _HomepageState extends State<Homepage> {
               SizedBox(height: 40),
 
               SizedBox(
-                height: 88,
-                child: ListView.builder(
-                  itemCount: 10,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) => Stack(
-                    alignment: Alignment.topCenter,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        // height: 88,
-                        width: 87,
-                        decoration: BoxDecoration(
-                          color: Color(0xffF2FCF4),
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Container(
-                              height: 38,
-                              width: 87,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  bottomRight: Radius.circular(10),
-                                  bottomLeft: Radius.circular(10),
-                                ),
+                height: 120,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ListView.builder(      clipBehavior: Clip.none,
 
-                                color: Color(0xff55AB60),
-                              ),
-                              child: Center(
-                                child: CustomTextWidget(
-                                  title: "Groecries",
-                                  fs: 12,
-                                  fw: FontWeight.w600,
-                                  fc: Colors.white,
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) => Stack(
+                      alignment: Alignment.topCenter,
+                      clipBehavior: Clip.none,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(5),
+                          // height: 88,
+                          width: 87,
+                          decoration: BoxDecoration(
+                            color: Color(0xffF2FCF4),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Container(
+                                height: 38,
+                                width: 87,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    bottomRight: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                  ),
+
+                                  color: Color(0xff55AB60),
+                                ),
+                                child: Center(
+                                  child: CustomTextWidget(
+                                    title: "Groecries",
+                                    fs: 12,
+                                    fw: FontWeight.w600,
+                                    fc: Colors.white,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Positioned(
-                        bottom: 40,
-                        left: 10,
+                        Positioned(
+                          bottom: 60,
+                          left: 10,
+                          child: Image.asset("assets/apple.png"),
 
-                        child: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjNSiND1mtJ4c1x5AHwWfh1JhJVDR2042FhA&s",
-                          width: 75,
-                          height: 65,
+                          // child: Image.network(
+                          //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjNSiND1mtJ4c1x5AHwWfh1JhJVDR2042FhA&s",
+                          //   width: 75,
+                          //   height: 65,
+                          // ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -125,29 +131,61 @@ class _HomepageState extends State<Homepage> {
                   ),
                 ],
               ),
-              Container(
-                height: 192,
-                width: 162.06,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9oGtW-OcPE9oEKhTlgKad_MGo_nwD3780uA&s",
-                          width: 86,
-                          height: 92,
-                        ),
+              SizedBox(height: 20),
+              Stack(
+                children: [
+                  Container(
+                    height: 192,
+                    width: 162.06,
+                    decoration: BoxDecoration(
+                      color: Color(0xffF2FCF4),
+
+                      //Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                            spacing: 8,
+                        children: [
+                          SizedBox(height:5),
+                          Center(
+                            child: Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ66ubNfMxRuPH4cP2X5XvaF_lU3aKILmR9VQ&s",
+                              width: 86,
+                              height: 92,
+                            ),
+                          ),
+                          CustomTextWidget(
+                            title: "Fortune rice",
+                            fs: 16,
+                            fw: FontWeight.w500,
+                          ),
+                          CustomTextWidget(
+                            title: "\$3/kg",
+                            fs: 16,
+                            fw: FontWeight.w700,
+                          ),
+                        ],
                       ),
-                      CustomTextWidget(title: "Fortune rice",fs: 16,fw: FontWeight.w500,),
-                      CustomTextWidget(title: "\$3/kg",fs: 16,fw: FontWeight.w700,)
-                    ],
+                    ),
                   ),
-                ),
+                  Positioned(top: 10
+                    ,
+                    child: Container(
+                      height: 41,
+                      width: 58.17,
+                      decoration: BoxDecoration(
+                        color: Color(0xff55AB60),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),child: Center(child: CustomTextWidget(title: "37% \n Off",fs: 14,fw: FontWeight.w600,fc: Colors.white,)),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
