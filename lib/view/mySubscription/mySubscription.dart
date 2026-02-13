ea8c972b1c5dad9e4f3cd33817c0203aae3399ac
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-13 23:18:27 +0600

    My Subscriptions

--- lib/view/mySubscription/mySubscription.dart
+++ lib/view/mySubscription/mySubscription.dart
@@ -58,6 +58,7 @@ class _MySubscriptionState extends State<MySubscription> {
               },
             ),
           ),
+
           Expanded(
             child: Container(
               height: 728,
@@ -72,7 +73,7 @@ class _MySubscriptionState extends State<MySubscription> {
               child: Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.only(top: 20, left: 20),
                     child: Row(
                       spacing: 10,
                       children: [
@@ -95,124 +96,142 @@ class _MySubscriptionState extends State<MySubscription> {
                       ],
                     ),
                   ),
-                 Expanded(
-                   child: ListView.builder(itemCount: 3,
 
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 3,
 
-                     itemBuilder: (i,c)=> Stack(
-                     children: [
-                       Card(
-                         color: Colors.white,
-                         child: Padding(
-                           padding: const EdgeInsets.all(10),
-                           child: Row(
-                             crossAxisAlignment: CrossAxisAlignment.start,
-                             spacing: 10,
-                             children: [
-                               Image(image: AssetImage("assets/fortune.png")),
-                               Column(
-                                 spacing: 5,
-                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                 children: [
-                                   Row(//mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                     spacing: 120,
-                                     children: [
-                                       CustomTextWidget(
-                                         title: "Fortune rice",
-                                         fs: 16,
-                                         fw: FontWeight.w600,
-                                         fc: Colors.grey[700],
-                                       ),
-                                       CustomTextWidget(
-                                         title: "Qty : 2",
-                                         fw: FontWeight.w600,
-                                         fs: 14,
-                                         fc: Colors.grey[600],
-                                       ),
-                                     ],
-                                   ),
-                                   CustomTextWidget(
-                                     title: "1 kg",
-                                     fw: FontWeight.w400,
-                                     fs: 14,
-                                     fc: Colors.grey,
-                                   ),
-                                   CustomTextWidget(
-                                     title: "\$3",
-                                     fs: 18,
-                                     fw: FontWeight.w700,
-                                     fc: Colors.grey[800],
-                                   ),
-                                 ],
-                               ),
-                             ],
-                           ),
-                         ),
-                       ),
-                       Positioned(bottom: 4,right: 4,
-                         child: Container(
-                           width: 159,
-                           height: 43,
-                           decoration: BoxDecoration(
-                             color: Color(0xff55AB60),
-                             borderRadius: BorderRadius.only(
-                               topLeft: Radius.circular(10),
-                               bottomRight: Radius.circular(10),
-                             ),
-                           ),
-                           child: Padding(
-                             padding: const EdgeInsets.all(8.0),
-                             child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                               children: [
-                                 CustomTextWidget(
-                                   title: "M",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                                 CustomTextWidget(
-                                   title: "T",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                                 CustomTextWidget(
-                                   title: "W",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                                 CustomTextWidget(
-                                   title: "T",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                                 CustomTextWidget(
-                                   title: "F",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                                 CustomTextWidget(
-                                   title: "S",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                                 CustomTextWidget(
-                                   title: "S",
-                                   fs: 14,
-                                   fw: FontWeight.w700,
-                                   fc: Colors.white,
-                                 ),
-                               ],
-                             ),
-                           ),
-                         ),)
-                     ],
-                   ),),
-                 )
+                      itemBuilder: (i, c) => Stack(
+                        children: [
+                          Card(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                spacing: 10,
+                                children: [
+                                  Image(
+                                    image: AssetImage("assets/fortune.png"),
+                                  ),
+                                  Column(
+                                    spacing: 5,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        spacing: 120,
+                                        children: [
+                                          CustomTextWidget(
+                                            title: "Fortune rice",
+                                            fs: 16,
+                                            fw: FontWeight.w600,
+                                            fc: Colors.grey[700],
+                                          ),
+                                          CustomTextWidget(
+                                            title: "Qty : 2",
+                                            fw: FontWeight.w600,
+                                            fs: 14,
+                                            fc: Colors.grey[600],
+                                          ),
+                                        ],
+                                      ),
+                                      CustomTextWidget(
+                                        title: "1 kg",
+                                        fw: FontWeight.w400,
+                                        fs: 14,
+                                        fc: Colors.grey,
+                                      ),
+                                      CustomTextWidget(
+                                        title: "\$3",
+                                        fs: 18,
+                                        fw: FontWeight.w700,
+                                        fc: Colors.grey[800],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 4,
+                            right: 4,
+                            child: Container(
+                              width: 159,
+                              height: 43,
+                              decoration: BoxDecoration(
+                                color: Color(0xff55AB60),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    CustomTextWidget(
+                                      title: "M",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                    CustomTextWidget(
+                                      title: "T",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                    CustomTextWidget(
+                                      title: "W",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                    CustomTextWidget(
+                                      title: "T",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                    CustomTextWidget(
+                                      title: "F",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                    CustomTextWidget(
+                                      title: "S",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                    CustomTextWidget(
+                                      title: "S",
+                                      fs: 14,
+                                      fw: FontWeight.w700,
+                                      fc: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Image(image: AssetImage("assets/exit.png")),
+                  CustomTextWidget(
+                    title: "Excited to serve you the best quality",
+                    fw: FontWeight.w400,
+                    fs: 15,fc: Colors.grey[600],
+                  ),//😣😣😣😣😣😣😣😣😣😣😣😣😣😣problem
+                  SizedBox(height: 100,)
                 ],
               ),
             ),
