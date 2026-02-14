27ddbda5be794eef180416d52f3df4f2cd4679dd
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-14 23:53:01 +0600

    My cart ,Payment details

--- lib/view/myCart/myCart.dart
+++ lib/view/myCart/myCart.dart
@@ -63,12 +63,14 @@ class _MyCartScreenState extends State<MyCartScreen> {
                   ),
                 ),
               ),
-              SizedBox(height: 270,
+              SizedBox(
+                height: 270,
                 child: ListView.builder(
                   itemCount: 2,
                   itemBuilder: (i, c) => CouponCardWidget(),
                 ),
-              ),SizedBox(height: 10,),
+              ),
+              SizedBox(height: 10),
               CustomTextWidget(
                 title: "Promo code can be applied on payment screen",
                 fs: 16,
@@ -76,6 +78,107 @@ class _MyCartScreenState extends State<MyCartScreen> {
                 fc: Color(0xffA4E0A4),
                 maxLine: 2,
                 textA: TextAlign.center,
+              ),
+              CustomTextWidget(
+                title: "Payment details",
+                fw: FontWeight.w700,
+                fs: 22,
+                fc: Colors.grey[800],
+              ),
+              SizedBox(height: 10,),
+              Card(color: Colors.white,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CustomTextWidget(
+                            title: "Total MRP",
+                            fw: FontWeight.w400,
+                            fs: 16,
+                            fc: Colors.grey[600],
+                          ),
+                          CustomTextWidget(
+                            title: "\$ 9.97",
+                            fw: FontWeight.w400,
+                            fs: 16,
+                            fc: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CustomTextWidget(
+                            title: "Discount",
+                            fw: FontWeight.w400,
+                            fs: 16,
+                            fc: Colors.grey[600],
+                          ),
+                          CustomTextWidget(
+                            title: "\$ 0.00",
+                            fw: FontWeight.w400,
+                            fs: 1,
+                            fc: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CustomTextWidget(
+                            title: "Shipping Charges",
+                            fw: FontWeight.w400,
+                            fs: 16,
+                            fc: Colors.grey[600],
+                          ),
+                          CustomTextWidget(
+                            title: "Free",
+                            fw: FontWeight.w400,
+                            fs: 16,
+                            fc: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      height: 40,
+                      width: 396,
+                      decoration: BoxDecoration(
+                        color: Color(0xffDCFFE2),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      ),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CustomTextWidget(
+                            title: "Total",
+                            fw: FontWeight.w600,
+                            fs: 18,
+                            fc: Colors.grey[800],
+                          ),
+                          CustomTextWidget(
+                            title: "\$ 9.97",
+                            fw: FontWeight.w600,
+                            fs: 18,
+                            fc: Colors.grey[800],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
