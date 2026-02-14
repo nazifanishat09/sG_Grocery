d438a57cd7df1f32b3c66f829e517286848cd1f3
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-14 22:44:32 +0600

    My cart ,Coupon Card

--- lib/view/myCart/myCart.dart
+++ lib/view/myCart/myCart.dart
@@ -22,6 +22,7 @@ class _MyCartScreenState extends State<MyCartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: Image(image: AssetImage("assets/backArrow.png")),
         centerTitle: true,
@@ -30,6 +31,7 @@ class _MyCartScreenState extends State<MyCartScreen> {
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             DropdownButton<String>(
@@ -49,11 +51,209 @@ class _MyCartScreenState extends State<MyCartScreen> {
                 setState(() {});
               },
             ),
-            Expanded(
-              child: ListView.builder(itemCount: 3,
+            // SizedBox(
+            //   height: 350,
+            //   child: Expanded(
+            //     child: ListView.builder(
+            //       itemCount: 3,
+            //
+            //       itemBuilder: (i, c) => NewCardWidget(),
+            //     ),
+            //   ),
+            // ),
 
-                itemBuilder: (i,c)=> NewCardWidget(),),
-            )
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Row(
+                  spacing: 1,
+                  children: [
+                    Container(
+                      width: 270,
+                      height: 127,
+                      decoration: BoxDecoration(
+                        color: Color(0xff60C370),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          //crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+
+                              children: [
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                                SizedBox(height: 55),
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                              ],
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10,top: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CustomTextWidget(
+                                    title: "FLAT",
+                                    fs: 14,
+                                    fw: FontWeight.w700,
+                                    fc: Colors.white,
+                                  ),
+                                  CustomTextWidget(
+                                    title: "50% off",
+                                    fs: 20,
+                                    fw: FontWeight.w700,
+                                    fc: Colors.white,
+                                  ),
+                                  CustomTextWidget(
+                                    title: "on your first order",
+                                    fs: 12,
+                                    fw: FontWeight.w400,
+                                    fc: Colors.white,
+                                  ),
+                                  CustomTextWidget(
+                                    title: "Use coupon code to get OFFER",
+                                    fs: 12,
+                                    fw: FontWeight.w400,
+                                    fc: Colors.white,
+                                  ),
+                                ],
+                              ),
+                            ),SizedBox(width:  20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+
+                              children: [
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                                SizedBox(height:54),
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 90,
+                      height: 127,
+                      decoration: BoxDecoration(
+                        color: Color(0xff60C370),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8,),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+
+                              children: [
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                                SizedBox(height: 38),
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                CustomTextWidget(
+                                  title: "code",
+                                  fs: 14,
+                                  fw: FontWeight.w400,
+                                  fc: Colors.white,
+                                ),
+                                CustomTextWidget(
+                                  title: "GETFIRST",
+                                  fs: 16,
+                                  fw: FontWeight.w700,
+                                  fc: Colors.white,
+                                ),
+                              ],
+                            ),
+                           // SizedBox(height: 2),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+
+                              children: [
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                                SizedBox(height: 38),
+                                Icon(
+                                  Icons.close,size: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xffA4E0A4),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  right: 190,
+                  top: -18,
+                  child: Column(
+                    spacing: 100,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        child: CircleAvatar(backgroundColor: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 30,
+                        child: CircleAvatar(backgroundColor: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+
+
+
 
           ],
         ),
@@ -61,5 +261,3 @@ class _MyCartScreenState extends State<MyCartScreen> {
     );
   }
 }
-
-
