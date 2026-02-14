d309ec8993f071105730975d68942eb9a5d7583e
nazifanishat09 <nazifanishat2024@gmail.com>
2026-02-14 13:32:45 +0600

    My cart ,drop down

--- lib/view/myCart/myCart.dart
+++ lib/view/myCart/myCart.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sg_grocery/view/widget/customAppber.dart';
 import 'package:sg_grocery/view/widget/custom_text.dart';
 
-
 class MyCartScreen extends StatefulWidget {
   const MyCartScreen({super.key});
 
@@ -11,20 +10,45 @@ class MyCartScreen extends StatefulWidget {
 }
 
 class _MyCartScreenState extends State<MyCartScreen> {
+  List nameL = [
+    "Today, 6 PM - 10 PM",
+    "Tomorrow, 7 AM - 9 PM",
+    "Pick a specific date",
+  ];
+  String? value;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      leading: Image(image: AssetImage("assets/backArrow.png")),centerTitle: true,
-      title: CustomTextWidget(title: "My Cart",fs: 22,fw: FontWeight.w700,) ,
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: Image(image: AssetImage("assets/backArrow.png")),
+        centerTitle: true,
+        title: CustomTextWidget(title: "My Cart", fs: 22, fw: FontWeight.w700),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(children: [
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              icon: Image(image: AssetImage("assets/dropdown.png")),
+              underline: SizedBox() ,
+              hint: CustomTextWidget(title: 'Tomorrow, 7 AM - 9 PM',fs: 10,),
+              value: value == "" ? null : value,
 
-        ],),
+              items: nameL
+                  .map(
+                    (v) => DropdownMenuItem<String>(value: v, child: Text(v)),
+                  )
+                  .toList(),
+
+              onChanged: (c) {
+                value = c;
+                setState(() {});
+              },
+            ),
+          ],
+        ),
       ),
-
-
     );
   }
 }
