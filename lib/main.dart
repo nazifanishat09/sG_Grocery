4a00bcac93cc14910223c2df8345751af6d1dc0f
nazifanishat09 <nazifanishat2024@gmail.com>
2026-03-05 16:26:42 +0600

    IntlPhoneField

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyCartScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: PaymentScreen());
   }
 }
--- lib/view/paymentScreen/paymentScreen.dart
+++ lib/view/paymentScreen/paymentScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sg_grocery/view/widget/customAppber.dart';
 import 'package:sg_grocery/view/widget/custom_text.dart';
 import 'package:sg_grocery/view/widget/textFormField.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PaymentScreen extends StatefulWidget {
   const PaymentScreen({super.key});
@@ -11,6 +13,9 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
+  bool active = true
+  ;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +29,8 @@ class _PaymentScreenState extends State<PaymentScreen> {
           Expanded(
             child: Container(
               padding: EdgeInsets.all(10),
-              width: 428,
-              height: 1958,
+              width: 428.w,
+              height: 1958.h,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -34,22 +39,76 @@ class _PaymentScreenState extends State<PaymentScreen> {
                 ),
               ),
               child: Column(
+                spacing: 10.h,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   CustomTextWidget(
                     title: "Contact Details",
                     fw: FontWeight.w600,
-                    fs: 22,
+                    fs: 22.sp,
                     fc: Colors.grey[800],
                   ),
                   TextFormFieldWidget(
-                    border: OutlineInputBorder(borderSide: BorderSide.none,borderRadius: BorderRadius.circular(10)),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                     title: "First name",
                     kyboard: TextInputType.text,
                     bgC: Colors.grey[200],
                     bC: Colors.grey[200],
-                    radius: 10,
+                    radius: 10.r,
                   ),
+                  TextFormFieldWidget(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    title: "Last name",
+                    kyboard: TextInputType.text,
+                    bgC: Colors.grey[200],
+                    bC: Colors.grey[200],
+                    radius: 10.r,
+                  ),
+                  TextFormFieldWidget(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    title: "Email",
+                    kyboard: TextInputType.emailAddress,
+                    bgC: Colors.grey[200],
+                    bC: Colors.grey[200],
+                    radius: 10.r,
+                  ),
+                  IntlPhoneField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                      labelText: 'Phone Number',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    initialCountryCode: 'A',
+                    onChanged: (phone) {
+                      print(phone.completeNumber);
+                    },
+                  ),
+
+                  CustomTextWidget(
+                    title: "Choose Delivery Address",
+                    fs: 22,
+                    fw: FontWeight.w600,
+                  ),
+                  Radio(activeColor: Colors.green,
+                      value: active,groupValue: active, onChanged: (a) {active = a!;
+                    setState(() {
+
+                    });
+
+                  }),
                 ],
               ),
             ),
